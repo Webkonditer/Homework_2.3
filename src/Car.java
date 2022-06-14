07fcd7ad3cb7a148afca09be280e7b2d09f1adb9
6564747347456845 <webkonditer@yandex.ru>
2022-06-14 20:34:00 +0700

    Done

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car extends Vehicle implements ServiceStationInterfase {
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+        this.checkEngine();
+    }
+}
---
+++ src/ServiceStationInterfase.java
@@ -0,0 +1,5 @@
+public interface ServiceStationInterfase {
+
+    public void check();
+
+}
